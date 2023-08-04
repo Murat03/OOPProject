a06c4b5d5b7b9fe840f73dd9f1aa061f2df7a35f
Murat03 <muratipekk033@gmail.com>
2023-08-04 16:15:10 +0300

    ICloneable

---
+++ OOP.Interfaces.ICloneableApp/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Interfaces.ICloneableApp
+{
+    public class Book : ICloneable
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+
+        public object Clone()
+        {
+            ////deep copy
+            //var book = new Book();
+            //book.Id = Id;
+            //book.Title = Title;
+            //book.Price = Price;
+            //return book;
+
+            //shallow copy
+            return MemberwiseClone();
+        }
+    }
+}
---
+++ OOP.Interfaces.ICloneableApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OOP.Interfaces.ICloneableApp;
+
+var book1 = new Book()
+{
+    Id = 1,
+    Title = "Satranç",
+    Price = 20
+};
+var book2 = book1.Clone() as Book;
+book2.Title = "Utopia";
+
+Console.WriteLine(book1.Title);
+Console.WriteLine(book2.Title);
