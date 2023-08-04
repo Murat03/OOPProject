75d223a3ccadb6a5f5b0299bfbd6827d132755c7
Murat03 <muratipekk033@gmail.com>
2023-08-04 11:35:09 +0300

    Interface Implementation

---
+++ Interfaces/Contracts/IBankAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Contracts
+{
+    public interface IBankAccount
+    {
+        decimal Balance { get; }
+        void PayIn(decimal amount);
+        bool Withdraw(decimal amount);
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Interfaces;
+
+Console.WriteLine("Hello, World!");
+
+SaverAccount saverAccount = new SaverAccount();
+
+saverAccount.PayIn(100);
+saverAccount.PayIn(200);
+saverAccount.PayIn(150);
+saverAccount.Withdraw(120);
+saverAccount.Withdraw(1200);
+Console.WriteLine(saverAccount);
