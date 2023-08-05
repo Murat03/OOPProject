5a3a302d0f73197c842750d1d487bce1c0e89447
Murat03 <muratipekk033@gmail.com>
2023-08-05 08:59:26 +0300

    Generic IComparable

---
+++ OOP.Interfaces.IComparable_T/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Interfaces.IComparable_T
+{
+    public class Employee : IComparable<Employee>
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public decimal Salary { get; set; }
+
+        public int CompareTo(Employee? other)
+        {
+            return this.FullName.CompareTo(other.FullName);
+        }
+
+        public override string ToString()
+        {
+            return $"{Id,-5} {FullName, -25} {Salary, -7}";
+        }
+    }
+}
---
+++ OOP.Interfaces.IComparable_T/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OOP.Interfaces.IComparable_T;
+
+var employees = new List<Employee>()
+{
+    new Employee()
+    {
+        Id = 1,
+        FullName = "Mehmet Yılmaz",
+        Salary = 9500
+    },
+    new Employee()
+    {
+        Id=2,
+        FullName = "Ali Sunal",
+        Salary = 15000
+    },
+    new Employee()
+    {
+        Id = 3,
+        FullName = "Mustafa Kılıçdar",
+        Salary = 20000
+    },
+    new Employee()
+    {
+        Id = 4,
+        FullName = "Mahmut Erdoğan",
+        Salary = 2000
+    },
+    new Employee()
+    {
+        Id = 5,
+        FullName = "Merve Haznedar",
+        Salary = 30000
+    }
+};
+employees.Sort();
+foreach (var employee in employees)
+{
+    Console.WriteLine(employee);
+}
+
+Console.ReadKey();
